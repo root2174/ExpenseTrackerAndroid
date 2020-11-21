931cb0eedab7b3459e5fbf046bcd6485de779d48
Lucas Magalhaes <lm031719@gmail.com>
2020-11-21 16:10:46 -0300

    auth with firebase

--- app/src/main/java/com/example/expensetracker/activities/LoginActivity.kt
+++ app/src/main/java/com/example/expensetracker/activities/LoginActivity.kt
@@ -1,13 +1,17 @@
 package com.example.expensetracker.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.expensetracker.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -15,10 +19,13 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var password: EditText
     private lateinit var loginButton: Button
     private lateinit var goToRegister: TextView
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
 
         // Finding Views
         email = findViewById(R.id.login_email)
@@ -37,6 +44,35 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
                 val registerActivityIntent = Intent(this, RegisterActivity::class.java)
                 finish()
                 startActivity(registerActivityIntent)
+            }
+
+            R.id.login_button -> {
+                var formOk = true
+                if (email.text.isEmpty()) {
+                    email.error = "Email cannot be empty!"
+                    formOk = false
+                }
+
+                if (password.text.isEmpty()) {
+                    password.error = "Password cannot be empty!"
+                    formOk = false
+                }
+
+                if (formOk) {
+                    val emailText = email.text.toString().trim()
+                    val passwordText = password.text.toString().trim()
+
+                    auth.signInWithEmailAndPassword(emailText, passwordText)
+                        .addOnCompleteListener(this) {task ->
+                            if (task.isSuccessful) {
+                                val it = Intent(this, MainActivity::class.java)
+                                startActivity(it)
+                            } else {
+                                Toast.makeText(this, "Unable to login!", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                }
             }
         }
     }
--- app/src/main/java/com/example/expensetracker/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/expensetracker/activities/RegisterActivity.kt
@@ -7,7 +7,11 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.example.expensetracker.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -16,9 +20,13 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var register: Button
     private lateinit var goToLogin: TextView
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        auth = Firebase.auth
 
         // Finding Views
         email = findViewById(R.id.register_email)
@@ -37,6 +45,36 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
                 val loginActivityIntent = Intent(this, LoginActivity::class.java)
                 finish()
                 startActivity(loginActivityIntent)
+            }
+
+            R.id.register_button -> {
+                var formOk = true
+                if (email.text.isEmpty()) {
+                    email.error = "Email cannot be empty!"
+                    formOk = false
+                }
+
+                if (password.text.isEmpty()) {
+                    password.error = "Password cannot be empty!"
+                    formOk = false
+                }
+
+                if (formOk) {
+                    val emailText = email.text.toString().trim()
+                    val passwordText = password.text.toString().trim()
+                    auth.createUserWithEmailAndPassword(emailText, passwordText)
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(this, "User created!", Toast.LENGTH_SHORT)
+                                    .show()
+                            } else {
+                                Toast.makeText(this, "Could not create user!", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+
+                        }
+
+                }
             }
         }
     }
