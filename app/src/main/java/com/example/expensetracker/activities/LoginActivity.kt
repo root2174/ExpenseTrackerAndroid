2f531140cbdffaf4fdc8bef14a06b7f4c0541fb6
Lucas Magalhaes <lm031719@gmail.com>
2020-11-21 14:40:31 -0300

    naviation between login and register screens

--- app/src/main/java/com/example/expensetracker/activities/LoginActivity.kt
+++ app/src/main/java/com/example/expensetracker/activities/LoginActivity.kt
@@ -1,12 +1,43 @@
 package com.example.expensetracker.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.expensetracker.R
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var loginButton: Button
+    private lateinit var goToRegister: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Finding Views
+        email = findViewById(R.id.login_email)
+        password = findViewById(R.id.login_password)
+        loginButton = findViewById(R.id.login_button)
+        goToRegister = findViewById(R.id.login_goToRegister)
+
+        // Setting onClickListeners
+        goToRegister.setOnClickListener(this)
+        loginButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.login_goToRegister -> {
+                val registerActivityIntent = Intent(this, RegisterActivity::class.java)
+                finish()
+                startActivity(registerActivityIntent)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/expensetracker/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/expensetracker/activities/RegisterActivity.kt
@@ -1,12 +1,43 @@
 package com.example.expensetracker.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.expensetracker.R
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var register: Button
+    private lateinit var goToLogin: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        // Finding Views
+        email = findViewById(R.id.register_email)
+        password = findViewById(R.id.register_password)
+        register = findViewById(R.id.register_button)
+        goToLogin = findViewById(R.id.register_goToLogin)
+
+        // Setting onClickListeners
+        goToLogin.setOnClickListener(this)
+        register.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.register_goToLogin -> {
+                val loginActivityIntent = Intent(this, LoginActivity::class.java)
+                finish()
+                startActivity(loginActivityIntent)
+            }
+        }
     }
 }
